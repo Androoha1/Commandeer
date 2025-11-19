d5dbd753326bccd516ab004b8f734c14e65d3834
Andrii Posternak <posternak.andrii1@gmail.com>
2025-11-19 11:11:33 +0200

    Add doc-block method hints to the Rector Builder class.

--- src/Builders/Rector.php
+++ src/Builders/Rector.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace Posternak\Commandeer\Builders;
 
+/**
+ * @method static self process(...$args)
+ * @method __clear_cache()
+ */
 final class Rector extends Builder {
     protected static function getExecutableName(): string
     {
