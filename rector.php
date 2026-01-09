2c2e362101c0a5a6eae5d061277b860be7b8309d
Andrii Posternak <posternak.andrii1@gmail.com>
2026-01-09 13:16:51 +0200

    Add rector base configuration.

---
+++ rector.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Caching\ValueObject\Storage\FileCacheStorage;
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__,
+    ])
+    ->withSkip([
+        __DIR__ . '/vendor',
+        __DIR__ . '/cache',
+    ])
+    ->withPreparedSets(
+        deadCode: true,
+        codeQuality: true,
+        codingStyle: true,
+        typeDeclarations: true,
+        privatization: true,
+        naming: true,
+        instanceOf: true,
+        earlyReturn: true,
+        carbon: true,
+        rectorPreset: true,
+        phpunitCodeQuality: true,
+    )
+    ->withCache(
+        cacheDirectory: __DIR__ . '/cache/rector',
+        cacheClass: FileCacheStorage::class
+    );
